8cf624d25813ae559ab7a39ca528747204082f02
Erwin <101582520+re48jr7q@users.noreply.github.com>
2023-04-30 11:51:52 +0200

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
+constexpr int greater(int x, int y)
+{
+	return (x > y ? x : y);
+}
+
 int main()
 {
-	
-	return 0;		
+	constexpr int g{ greater(5, 6) };
+	std::cout << g << " is greater!\n";	//case 1: evaluated at compile-time
+
+	int x{ 5 };
+	std::cout << greater(x, 6) << " is greater!\n";	//case 2: evaluated at
+													//run-time
+
+	std::cout << greater(5, 6) << " is greater!\n";	//case 3: evaluated at
+	return 0;									//compile-time or run-time
 }	
 
 
